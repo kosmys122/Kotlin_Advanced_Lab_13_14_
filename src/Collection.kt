77010ce1728be3822df1d1ba60c88dc83c5f7b5d
Шапкин Николай <kosmys122@gmail.com>
2026-05-28 20:04:12 +0300

    ap collection in otlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -3,12 +3,21 @@ val gasPlanets= arrayOf("Jupiter","Saturn","Uranus","Neptune")
 val solarSystem=rockPlanets+gasPlanets
 
 fun main() {
-    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem= mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+
+    )
     println(solarSystem.size)
-    println("Pluto" in solarSystem)
-    solarSystem.add("Pluto")
-    println(solarSystem.size)
-    solarSystem.remove("Pluto")
-    println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
+    solarSystem["Pluto"]=5
+    println(solarSystem["Pluto"])
+    println(solarSystem.get("Theia"))
+    solarSystem["Jupiter"]=78
+    println(solarSystem["Jupiter"])
 }
