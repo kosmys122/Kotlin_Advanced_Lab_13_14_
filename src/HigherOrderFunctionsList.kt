357b8bec94b91355ac36eabd8bfd0cd8306a298a
Шапкин Николай <kosmys122@gmail.com>
2026-05-28 20:16:59 +0300

    groupy() function

--- src/HigherOrderFunctionsList.kt
+++ src/HigherOrderFunctionsList.kt
@@ -50,11 +50,15 @@ val cookies = listOf(
 )
 
 fun main() {
-    val softBakedMenu=cookies.filter {
-        it.softBaked
+    val groupedMenu = cookies.groupBy { it.softBaked }
+    val softBakedMenu = groupedMenu[true] ?: emptyList()
+    val crunchyMenu = groupedMenu[false] ?: emptyList()
+    println("Soft cookies:")
+    softBakedMenu.forEach{
+        println("${it.name} - $${it.price}")
     }
-    println("ћ€гкое печенье:")
-    cookies.forEach{
-        println("ѕункт меню: ${it.name}")
+    println("Chunchy cookies:")
+    crunchyMenu.forEach {
+        println("${it.name} - $${it.price}")
     }
 }
