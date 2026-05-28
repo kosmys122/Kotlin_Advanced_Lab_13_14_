b94242b8106f3819fa0a03ab8d7fcde2446eacb9
Шапкин Николай <kosmys122@gmail.com>
2026-05-28 20:02:47 +0300

    ets in otlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -2,9 +2,13 @@ val rockPlanets= arrayOf<String>("Mercury","Venus","Earth","Mars")
 val gasPlanets= arrayOf("Jupiter","Saturn","Uranus","Neptune")
 val solarSystem=rockPlanets+gasPlanets
 
-fun main(){
-    val solarSystem= mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
-    for (planet in solarSystem){
-        println(planet)
-    }
+fun main() {
+    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
+    println("Pluto" in solarSystem)
+    solarSystem.add("Pluto")
+    println(solarSystem.size)
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
 }
